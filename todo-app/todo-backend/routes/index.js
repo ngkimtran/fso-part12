4d4cd8c76a2b001674721d475102f9529b28135a
Kim Tran <ngockimtranle@gmail.com>
2022-02-10 17:02:13 +0200

    ex: 12.8 - 12.12

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-const configs = require('../util/config')
+const configs = require('../util/config');
+const redis = require('../redis');
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
 router.get('/', async (req, res) => {
-  visits++
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
+  });
+});
+
+/* GET usage metadata. */
+router.get('/statistics', async (req, res) => {
+  res.send({
+    added_todos: Number(await redis.getAsync('added_todos')),
   });
 });
 
--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Todo } = require('../mongo');
 const router = express.Router();
+const redis = require('../redis');
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -14,6 +15,10 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false,
   });
+  await redis.setAsync(
+    'added_todos',
+    Number(await redis.getAsync('added_todos')) + 1
+  );
   res.send(todo);
 });
 
