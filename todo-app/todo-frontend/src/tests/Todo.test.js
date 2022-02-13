369fb42e3ca4b5fdfc744cee4bf3c5c87708e685
Kim Tran <ngockimtranle@gmail.com>
2022-02-13 14:53:55 +0200

    ex: 12.13 - 12.15

---
+++ todo-app/todo-frontend/src/tests/Todo.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent } from '@testing-library/react';
+import Todo from '../Todos/Todo';
+
+describe('A Todo component', () => {
+  let component;
+  const deleteHandler = jest.fn();
+  const completeHandler = jest.fn();
+  const todo = {
+    text: 'Testing todo',
+    done: false,
+  };
+
+  beforeEach(() => {
+    component = render(
+      <Todo
+        todo={todo}
+        onClickDelete={deleteHandler}
+        onClickComplete={completeHandler}
+      />
+    );
+  });
+
+  test('display todo text and status', () => {
+    const div = component.container.querySelector('div');
+    expect(div).toHaveTextContent('Testing todo');
+    expect(div).toHaveTextContent('This todo is not done');
+  });
+
+  test('can change the status of todo', () => {
+    const button = component.getByText('Set as done');
+    fireEvent.click(button);
+    expect(completeHandler).toHaveBeenCalledTimes(1);
+    expect(completeHandler.mock.calls[0][0]).toEqual({
+      text: 'Testing todo',
+      done: false,
+    });
+  });
+
+  test('can delete todo', () => {
+    const button = component.getByText('Delete');
+    fireEvent.click(button);
+    expect(deleteHandler).toHaveBeenCalledTimes(1);
+    expect(deleteHandler.mock.calls[0][0]).toEqual({
+      text: 'Testing todo',
+      done: false,
+    });
+  });
+});
